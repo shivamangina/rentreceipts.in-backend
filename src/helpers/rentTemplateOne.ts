1e5a56520ef92b23d2880909b5688b670263d1c9
unknown <satyasandeep786@gmail.com>
2021-05-13 09:23:08 +0530

    working on font size issues : set html zoom attribute to 0.5

--- src/helpers/rentTemplateOne.ts
+++ src/helpers/rentTemplateOne.ts
@@ -6,7 +6,7 @@ export const template = (data: any[]) => {
       
       <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css?family=Cinzel|Montserrat&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -21,7 +21,7 @@ export const template = (data: any[]) => {
         } 
 
         html{
-          zoom: 0.7;
+          zoom: 0.5;
         }
 
         @media print {
